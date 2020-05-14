109a7f4daf6e1f5dbab31f2e6eae74cd5c9e780b
jeyansaran620 <jeyansaran620@gmail.com>
2020-05-14 12:59:00 +0530

    improving folder

--- src/App.js
+++ src/App.js
@@ -13,6 +13,27 @@ class ContactList extends Component
       }  
 }
 
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+ ];
+
 function App() {
   return (
     <div className="App">
