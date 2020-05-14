5046dbf92c3eb0e5823291ba861953c9289f837a
jeyansaran620 <jeyansaran620@gmail.com>
2020-05-14 16:17:10 +0530

    setting state to the app component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import ListContacts from './ListContacts'
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
- ];
-
-function App() {
+class App extends React.Component {
+  state = {
+    contacts : [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+     ]
+  } 
+  render(){
   return (
     <div className="App">
-    <ListContacts contacts={contacts}/>
+    <ListContacts contacts={this.state.contacts}/>
     </div>
   );
+  }
 }
 
 export default App;
